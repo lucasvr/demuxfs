b083f62f5b67266dbb19b351850b00ec4199b5c5
Lucas C. Villa Real <lucasvr@users.noreply.github.com>
2008-10-23 14:52:43 +0000

    Wrote fsutils_path_walk(), which resolves the pathname for a given dentry.

--- src/fsutils.h
+++ src/fsutils.h
@@ -1,6 +1,7 @@
 #ifndef __fsutils_h
 #define __fsutils_h
 
+char *fsutils_path_walk(struct dentry *dentry, char *buf, size_t size);
 void fsutils_dump_tree(struct dentry *dentry, int spaces);
 struct dentry *fsutils_has_children(struct dentry *dentry, char *name);
 struct dentry *fsutils_get_dentry(struct dentry *root, const char *path);
