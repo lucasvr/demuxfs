d6253a1041c3843425f219465494f7c32f6e7803
Lucas C. Villa Real <lucasvr@users.noreply.github.com>
2008-12-08 20:25:58 +0000

    Wrote parser for the TS_INFORMATION_DESCRIPTOR.

--- src/descriptors/parser_0xcd.c
+++ src/descriptors/parser_0xcd.c
@@ -28,9 +28,71 @@
  */
 #include "demuxfs.h"
 
+struct transmission_type_data {
+	uint8_t transmission_type_info;
+	uint8_t num_of_service;
+	uint16_t service_id;
+};
+
+struct formatted_descriptor {
+	uint8_t remote_control_key_id;
+	uint8_t length_of_ts_name;
+	uint8_t transmission_type_count;
+	char ts_name[256];
+};
+
 /* TS_INFORMATION_DESCRIPTOR parser */
 int descriptor_0xcd_parser(const char *payload, int len, struct dentry *parent, struct demuxfs_data *priv)
 {
+	struct formatted_descriptor f;
+	uint8_t offset, i, j;
+
+	if (len < 2) {
+		TS_WARNING("cannot parse descriptor %#x: contents smaller than 2 bytes (%d)", 0xfe, len);
+		return -1;
+	}
+	
+	f.remote_control_key_id = payload[0];
+	f.length_of_ts_name = payload[1] >> 2;
+	f.transmission_type_count = payload[1] & 0x03;
+	
+	for (i=0; i<f.length_of_ts_name; ++i)
+		f.ts_name[i] = payload[2+i];
+	f.ts_name[i] = '\0';
+	
+	struct dentry *dentry = CREATE_DIRECTORY(parent, "TRANSMISSION_INFORMATION");
+	CREATE_FILE_NUMBER(dentry, &f, remote_control_key_id);
+	CREATE_FILE_NUMBER(dentry, &f, length_of_ts_name);
+	CREATE_FILE_NUMBER(dentry, &f, transmission_type_count);
+	CREATE_FILE_STRING(dentry, &f, ts_name, XATTR_FORMAT_STRING);
+
+	offset = 2 + f.length_of_ts_name;
+	for (i=0; i<f.transmission_type_count; ++i) {
+		struct transmission_type_data t;
+		char transmission_name[32];
+		struct dentry *subdir;
+		
+		sprintf(transmission_name, "TRANSMISSION_%02d", i);
+		subdir = CREATE_DIRECTORY(dentry, transmission_name);
+
+		t.transmission_type_info = payload[offset];
+		t.num_of_service = payload[offset+1];
+		CREATE_FILE_NUMBER(subdir, &t, transmission_type_info);
+		CREATE_FILE_NUMBER(subdir, &t, num_of_service);
+
+		for (j=0; j<t.num_of_service; ++j) {
+			char service_name[32];
+			struct dentry *service_dir;
+			
+			sprintf(service_name, "SERVICE_ID_%02d", j);
+			service_dir = CREATE_DIRECTORY(subdir, service_name);
+
+			t.service_id = (payload[offset+j+2] << 8) | payload[offset+j+3];
+			CREATE_FILE_NUMBER(service_dir, &t, service_id);
+		}
+		offset += 2 + j + (t.num_of_service ? 2 : 0);
+	}
+
     return 0;
 }
 
