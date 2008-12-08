6dbff1810b747cdd90099eccbf1971ef086a0038
Lucas C. Villa Real <lucasvr@users.noreply.github.com>
2008-12-08 03:59:52 +0000

    Written hashtable_{lock,unlock}.

--- src/hash.h
+++ src/hash.h
@@ -8,6 +8,7 @@ struct hash_item {
 
 struct hash_table {
 	int size;
+	pthread_mutex_t mutex;
 	struct hash_item **items;
 };
 
@@ -16,5 +17,7 @@ void hashtable_destroy(struct hash_table *table);
 void *hashtable_get(struct hash_table *table, ino_t key);
 bool hashtable_add(struct hash_table *table, ino_t key, void *data);
 bool hashtable_del(struct hash_table *table, ino_t key);
+void hashtable_lock(struct hash_table *hash);
+void hashtable_unlock(struct hash_table *hash);
 
 #endif /* __hash_h */
