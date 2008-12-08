59cd30bb9dfcfc24498ca71af6729d0a2e574bea
Lucas C. Villa Real <lucasvr@users.noreply.github.com>
2008-12-08 01:54:56 +0000

    Added definitions to directory names so that they don't end up spread across the source code.

--- src/fsutils.h
+++ src/fsutils.h
@@ -1,6 +1,13 @@
 #ifndef __fsutils_h
 #define __fsutils_h
 
+#define FS_PES_FIFO_NAME    "PES"
+#define FS_PAT_NAME         "PAT"
+#define FS_PMT_NAME         "PMT"
+#define FS_NIT_NAME         "NIT"
+#define FS_DESCRIPTORS_NAME "Descriptors"
+#define FS_STREAMS_NAME     "Streams"
+
 char *fsutils_path_walk(struct dentry *dentry, char *buf, size_t size);
 void fsutils_dump_tree(struct dentry *dentry, int spaces);
 struct dentry *fsutils_has_children(struct dentry *dentry, char *name);
