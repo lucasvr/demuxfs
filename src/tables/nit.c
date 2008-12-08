e545ef7b04e29734073b8d24cc31b405d1c55b4b
lucasvr <lucasvr@ac0b248e-9ced-4581-99a4-cae81f09c613>
2008-12-08 04:28:07 +0000

    Added missed body from commit #56.

--- src/tables/nit.c
+++ src/tables/nit.c
@@ -55,7 +55,53 @@ int nit_parse(const struct ts_header *header, const char *payload, uint8_t paylo
 	/* Set hash key and check if there's already one version of this table in the hash */
 	nit->dentry.inode = TS_PACKET_HASH_KEY(header, nit);
 	current_nit = hashtable_get(priv->table, nit->dentry.inode);
-	hashtable_add(priv->table, nit->dentry.inode, nit);
 
+	/* Check whether we should keep processing this packet or not */                                    
+	if (! nit->current_next_indicator || (current_nit && current_nit->version_number == nit->version_number)) {
+		free(nit);                                                                                         
+		return 0;                                                                                          
+	}                                                                                                          
+
+	/* TODO: increment the directory number somehow to indicate that this is a new version */                  
+
+	/* TODO: check payload boundaries */                                                                       
+
+	/* Parse NIT specific bits */                                                                              
+	nit->reserved_4 = payload[8] >> 4;                                                                         
+	nit->network_descriptors_length = ((payload[8] << 8) | payload[9]) & 0x0fff;                               
+	nit->num_descriptors = descriptors_count(&payload[10], nit->network_descriptors_length);                   
+	nit_create_directory(nit, priv);                                                                           
+
+	struct dentry *descriptors_dentry = CREATE_DIRECTORY(&nit->dentry, FS_DESCRIPTORS_NAME);                   
+	descriptors_parse(&payload[10], nit->num_descriptors, descriptors_dentry, priv);                           
+
+	uint8_t offset = 10 + nit->network_descriptors_length;                                                     
+	nit->reserved_5 = payload[offset] >> 4;                                                                    
+	nit->transport_stream_loop_length = ((payload[offset] << 8) | payload[offset+1]) & 0x0fff;                 
+	offset += 2;                                                                                               
+
+	struct dentry *ts_dentry = CREATE_DIRECTORY(&nit->dentry, "TS_Information");                               
+	uint16_t i = 0, info_index = 0;                                                                            
+	while (i < nit->transport_stream_loop_length) {                                                            
+		char subdir[PATH_MAX];                                                                             
+		snprintf(subdir, sizeof(subdir), "info-%02d", info_index++);                                       
+		struct dentry *info_dentry = CREATE_DIRECTORY(ts_dentry, subdir);                                  
+
+		struct nit_ts_data ts_data;                                                                        
+		ts_data.transport_stream_id = (payload[offset] << 8) | payload[offset+1];                          
+		ts_data.original_network_id = (payload[offset+2] << 8) | payload[offset+3];                        
+		ts_data.reserved_future_use = payload[offset+4] >> 4;                                              
+		ts_data.transport_descriptors_length = ((payload[offset+4] << 8) | payload[offset+5]) & 0x0fff;    
+		ts_data.num_descriptors = descriptors_count(&payload[offset+6], ts_data.transport_descriptors_length);
+		CREATE_FILE_NUMBER(info_dentry, &ts_data, transport_stream_id);                                       
+		CREATE_FILE_NUMBER(info_dentry, &ts_data, original_network_id);                                       
+		CREATE_FILE_NUMBER(info_dentry, &ts_data, transport_descriptors_length);                              
+
+		descriptors_parse(&payload[offset+6], ts_data.num_descriptors, info_dentry, priv);                    
+		i += 6 + ts_data.transport_descriptors_length;                                                        
+		offset += 6 + ts_data.transport_descriptors_length;                                                   
+	}                                                                                                             
+
+	hashtable_add(priv->table, nit->dentry.inode, nit);
 	return 0;
 }
