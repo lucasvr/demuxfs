cdd0af57453f153afaed71c2728156975576083f
lucasvr <lucasvr@ac0b248e-9ced-4581-99a4-cae81f09c613>
2009-01-02 14:03:42 +0000

    Casting to uint64_t to avoid integer truncation.

--- src/byteops.h
+++ src/byteops.h
@@ -4,6 +4,6 @@
 #define CONVERT_TO_16(a,b)       (((a)<<8) | (b))
 #define CONVERT_TO_24(a,b,c)     (((a)<<16) | ((b)<<8) | (c))
 #define CONVERT_TO_32(a,b,c,d)   (((a)<<24) | ((b)<<16) | ((c)<<8) | (d))
-#define CONVERT_TO_40(a,b,c,d,e) ((CONVERT_TO_32(a,b,c,d) << 8) | (e))
+#define CONVERT_TO_40(a,b,c,d,e) ((((uint64_t)a)<<32) | ((b)<<24) | ((c)<<16) | (d)<<8 | (e))
 
 #endif /* __byteops_h */
