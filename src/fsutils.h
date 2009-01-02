77bc195d38d9af3ced727932d5429eb20e99e5b9
lucasvr <lucasvr@ac0b248e-9ced-4581-99a4-cae81f09c613>
2009-01-02 14:37:24 +0000

    Implemented required support for on-the-fly modification of file contents.

--- src/fsutils.h
+++ src/fsutils.h
@@ -46,82 +46,122 @@ void fsutils_dispose_node(struct dentry *dentry);
 		(_dentry)->parent = _parent; \
 		list_add_tail(&(_dentry)->list, &((_parent)->children));
 
+#define UPDATE_COMMON(_dentry,_new_contents,_new_size) \
+ 		pthread_mutex_lock(&_dentry->mutex); \
+ 		if (_dentry->size != _new_size) { \
+ 			free(_dentry->contents); \
+			_dentry->contents = strdup(_new_contents); \
+ 			_dentry->size = _new_size; \
+ 		} else \
+ 			memcpy(_dentry->contents, _new_contents, _dentry->size); \
+ 		pthread_mutex_unlock(&_dentry->mutex);
+
 #define CREATE_FILE_BIN(parent,header,member,_size) \
 	({ \
-		struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
-	 	_dentry->contents = malloc(_size); \
-	 	memcpy(_dentry->contents, (header)->member, _size); \
-		_dentry->name = strdup(#member); \
-		_dentry->size = _size; \
-		_dentry->mode = S_IFREG | 0444; \
-		CREATE_COMMON((parent),_dentry); \
-		xattr_add(_dentry, XATTR_FORMAT, XATTR_FORMAT_BIN, strlen(XATTR_FORMAT_BIN), false); \
+	 	struct dentry *_dentry = fsutils_get_child(parent, #member); \
+	 	if (_dentry) { \
+	 		UPDATE_COMMON(_dentry, (header)->member, _size); \
+		} else { \
+	 		_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
+	 		_dentry->contents = malloc(_size); \
+	 		memcpy(_dentry->contents, (header)->member, _size); \
+			_dentry->name = strdup(#member); \
+			_dentry->size = _size; \
+			_dentry->mode = S_IFREG | 0444; \
+			CREATE_COMMON((parent),_dentry); \
+			xattr_add(_dentry, XATTR_FORMAT, XATTR_FORMAT_BIN, strlen(XATTR_FORMAT_BIN), false); \
+	 	} \
 	 	_dentry; \
 	})
 
 #define CREATE_FILE_NUMBER(parent,header,member) \
 	({ \
 	 	uint64_t member64 = (uint64_t) (header)->member; \
-		struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
-		asprintf(&_dentry->contents, "%#04llx", member64); \
-		_dentry->name = strdup(#member); \
-		_dentry->size = strlen(_dentry->contents); \
-		_dentry->mode = S_IFREG | 0444; \
-		CREATE_COMMON((parent),_dentry); \
-		xattr_add(_dentry, XATTR_FORMAT, XATTR_FORMAT_NUMBER, strlen(XATTR_FORMAT_NUMBER), false); \
+	 	struct dentry *_dentry = fsutils_get_child(parent, #member); \
+	 	if (_dentry) { \
+	 		pthread_mutex_lock(&_dentry->mutex); \
+	 		free(_dentry->contents); \
+			asprintf(&_dentry->contents, "%#04llx", member64); \
+			_dentry->size = strlen(_dentry->contents); \
+	 		pthread_mutex_unlock(&_dentry->mutex); \
+	 	} else { \
+			_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
+			asprintf(&_dentry->contents, "%#04llx", member64); \
+			_dentry->name = strdup(#member); \
+			_dentry->size = strlen(_dentry->contents); \
+			_dentry->mode = S_IFREG | 0444; \
+			CREATE_COMMON((parent),_dentry); \
+			xattr_add(_dentry, XATTR_FORMAT, XATTR_FORMAT_NUMBER, strlen(XATTR_FORMAT_NUMBER), false); \
+	 	} \
 	 	_dentry; \
 	})
 
 #define CREATE_FILE_STRING(parent,header,member,fmt) \
 	({ \
-		struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
-		_dentry->contents = strdup((header)->member); \
-		_dentry->name = strdup(#member); \
-		_dentry->size = strlen(_dentry->contents); \
-		_dentry->mode = S_IFREG | 0444; \
-		CREATE_COMMON((parent),_dentry); \
-		xattr_add(_dentry, XATTR_FORMAT, fmt, strlen(fmt), false); \
+	 	struct dentry *_dentry = fsutils_get_child(parent, #member); \
+	 	if (_dentry) { \
+	 		UPDATE_COMMON(_dentry, (header)->member, strlen((header)->member)); \
+	 	} else { \
+			_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
+			_dentry->contents = strdup((header)->member); \
+			_dentry->name = strdup(#member); \
+			_dentry->size = strlen(_dentry->contents); \
+			_dentry->mode = S_IFREG | 0444; \
+			CREATE_COMMON((parent),_dentry); \
+			xattr_add(_dentry, XATTR_FORMAT, fmt, strlen(fmt), false); \
+	 	} \
 	 	_dentry; \
 	})
 
 #define CREATE_SYMLINK(parent,sname,target) \
 	({ \
-		struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
-		_dentry->contents = strdup(target); \
-		_dentry->name = strdup(sname); \
-		_dentry->mode = S_IFLNK | 0777; \
-		CREATE_COMMON((parent),_dentry); \
+	 	struct dentry *_dentry = fsutils_get_child(parent, sname); \
+	 	if (! _dentry) { \
+			struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
+			_dentry->contents = strdup(target); \
+			_dentry->name = strdup(sname); \
+			_dentry->mode = S_IFLNK | 0777; \
+			CREATE_COMMON((parent),_dentry); \
+	 	} \
 	 	_dentry; \
 	})
 
 #define CREATE_SNAPSHOT_FILE(parent,fname,pes_dentry) \
 	({ \
-		struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
-		_dentry->name = strdup(fname); \
-		_dentry->mode = S_IFREG | 0444; \
-	 	_dentry->borrowed_pes_dentry = pes_dentry; \
-		CREATE_COMMON((parent),_dentry); \
+	 	struct dentry *_dentry = fsutils_get_child(parent, fname); \
+	 	if (! _dentry) { \
+			_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
+			_dentry->name = strdup(fname); \
+			_dentry->mode = S_IFREG | 0444; \
+	 		_dentry->borrowed_pes_dentry = pes_dentry; \
+			CREATE_COMMON((parent),_dentry); \
+	 	} \
 	 	_dentry; \
 	})
 
 #define CREATE_FIFO(parent,fname) \
 	({ \
-		struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
-		_dentry->name = strdup(fname); \
-		_dentry->mode = S_IFREG | 0777; \
-		_dentry->fifo = fifo_init(MAX_TS_PACKETS_IN_A_FIFO); \
-		CREATE_COMMON((parent),_dentry); \
+	 	struct dentry *_dentry = fsutils_get_child(parent, fname); \
+	 	if (! _dentry) { \
+	 		_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
+	 		_dentry->name = strdup(fname); \
+	 		_dentry->mode = S_IFREG | 0777; \
+	 		_dentry->fifo = fifo_init(MAX_TS_PACKETS_IN_A_FIFO); \
+	 		CREATE_COMMON((parent),_dentry); \
+	 	} \
 	 	_dentry; \
 	})
 
 #define CREATE_DIRECTORY(parent,dname) \
 	({ \
-		struct dentry *_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
-		_dentry->name = strdup(dname); \
-		_dentry->mode = S_IFDIR | 0555; \
-		CREATE_COMMON((parent),_dentry); \
+	 	struct dentry *_dentry = fsutils_get_child(parent, dname); \
+	 	if (! _dentry) { \
+			_dentry = (struct dentry *) calloc(1, sizeof(struct dentry)); \
+			_dentry->name = strdup(dname); \
+			_dentry->mode = S_IFDIR | 0555; \
+			CREATE_COMMON((parent),_dentry); \
+	 	} \
 	 	_dentry; \
 	})
-
 
 #endif /* __fsutils_h */
