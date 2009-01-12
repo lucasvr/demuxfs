9e3674d919ff76feaebd315b9dd18c410084967b
Lucas C. Villa Real <lucasvr@users.noreply.github.com>
2009-01-12 02:24:52 +0000

    DSM-CC packets carry their own descriptors, thus a specific parser for those needs to be implemented.

---
+++ src/dsm-cc/descriptors/parser_0x01.c
@@ -0,0 +1,51 @@
+/* 
+ * Copyright (c) 2008, Lucas C. Villa Real <lucasvr@gobolinux.org>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of GoboLinux nor the names of its contributors may
+ * be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "demuxfs.h"
+#include "fsutils.h"
+#include "xattr.h"
+#include "ts.h"
+
+struct formatted_descriptor {
+	char *text_char;
+};
+
+/* TYPE_DESCRIPTOR parser */
+int dsmcc_descriptor_0x01_parser(const char *payload, int len, struct dentry *parent, struct demuxfs_data *priv)
+{
+	struct formatted_descriptor f;
+	struct dentry *subdir = CREATE_DIRECTORY(parent, "TYPE");
+
+	f.text_char = malloc(len+1);
+	memcpy(f.text_char, payload, len);
+	f.text_char[len] = 0;
+	CREATE_FILE_STRING(subdir, &f, text_char, XATTR_FORMAT_STRING);
+
+	return 0;
+}
+
