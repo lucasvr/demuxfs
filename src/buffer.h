91f0a31a9273fc8f83b5aa4586045a59e1cc0f7f
lucasvr <lucasvr@ac0b248e-9ced-4581-99a4-cae81f09c613>
2009-05-22 15:02:33 +0000

    API change; Introduced a continuity counter field.

--- src/buffer.h
+++ src/buffer.h
@@ -8,16 +8,18 @@ struct buffer {
 	char *data;
 	size_t max_size;
 	size_t current_size;
+	uint8_t continuity_counter;
 	bool holds_pes_data;
 	bool pes_unbounded_data;
 };
 
 struct buffer *buffer_create(size_t max_size, bool pes_data);
 void buffer_destroy(struct buffer *buffer);
-int buffer_append(struct buffer *buffer, const char *buf, size_t size);
+int  buffer_append(struct buffer *buffer, const char *buf, size_t size);
+int  buffer_get_max_size(struct buffer *buffer);
+int  buffer_get_current_size(struct buffer *buffer);
 void buffer_reset_size(struct buffer *buffer);
-void buffer_reset_full(struct buffer *buffer);
-bool buffer_unbounded(struct buffer *buffer);
+bool buffer_is_unbounded(struct buffer *buffer);
 bool buffer_contains_full_psi_section(struct buffer *buffer);
 bool buffer_contains_full_pes_section(struct buffer *buffer);
 
