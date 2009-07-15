a1a928b627f5e2dabce43b7ddf53b406aa05d67e
Lucas C. Villa Real <lucasvr@users.noreply.github.com>
2009-07-15 17:28:35 +0000

    Initial parser for DSI.

---
+++ src/dsm-cc/dsi.h
@@ -0,0 +1,49 @@
+#ifndef __dsi_h
+#define __dsi_h
+
+/**
+ * DSI - Download Server Initiate
+ */
+
+struct dsi_group_info_indication {
+	uint16_t number_of_groups;
+	struct dsi_group_info {
+		uint32_t group_id;
+		uint32_t group_size;
+		// GroupCompatibility()
+		struct dsmcc_compatibility_descriptor group_compatibility;
+		uint16_t group_info_length;
+		char *group_info_bytes;
+	} *dsi_group_info;
+	uint16_t private_data_length;
+	/* the private bytes actually hold a service gateway info structure */
+	struct dsi_service_gateway_info {
+		// IOP::IOR()
+		uint8_t download_taps_count;
+		char *download_taps;
+		uint8_t service_context_list_count;
+		char *service_context_list;
+		uint16_t user_info_length;
+		char *user_info;
+	} service_gateway_info;
+};
+ 
+struct dsi_table {
+	/* struct dentry always comes first */
+	struct dentry *dentry;
+	/* common PSI header */
+	PSI_HEADER();
+	/* DSI specific bits */
+	struct dsmcc_message_header dsmcc_message_header;
+	char server_id[20];
+	struct dsmcc_compatibility_descriptor compatibility_descriptor;
+	uint16_t private_data_length;
+	/* the private bytes actually hold the group info indication */
+	struct dsi_group_info_indication group_info_indication;
+	uint32_t crc;
+} __attribute__((__packed__));
+
+int dsi_parse(const struct ts_header *header, const char *payload, uint32_t payload_len,
+		struct demuxfs_data *priv);
+
+#endif /* __dsi_h */
