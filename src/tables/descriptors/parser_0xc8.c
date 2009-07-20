759e5e4236921b09310294d6d0e4dd184137bbaf
Lucas C. Villa Real <lucasvr@users.noreply.github.com>
2009-07-20 18:09:58 +0000

    Parsing the VIDEO_DECODE_CONTROL_DESCRIPTOR.

--- src/tables/descriptors/parser_0xc8.c
+++ src/tables/descriptors/parser_0xc8.c
@@ -30,10 +30,55 @@
 #include "fsutils.h"
 #include "xattr.h"
 #include "ts.h"
+#include "descriptors.h"
+
+struct formatted_descriptor {
+	uint8_t still_picture_flag:1;
+	uint8_t sequence_end_code_flag:1;
+	uint8_t _video_encode_format:4;
+	char video_encode_format[64];
+	uint8_t reserved:2;
+};
 
 /* VIDEO_DECODE_CONTROL_DESCRIPTOR parser */
 int descriptor_0xc8_parser(const char *payload, int len, struct dentry *parent, struct demuxfs_data *priv)
 {
-    return -ENOSYS;
+	struct dentry *dentry;
+	struct formatted_descriptor f;
+	char video_fmt[64];
+
+	if (! descriptor_is_parseable(parent, payload[0], 3, len))
+		return -ENODATA;
+
+	dentry = CREATE_DIRECTORY(parent, "VIDEO_DECODE_CONTROL");
+	f.still_picture_flag = (payload[2] & 0x80) ? 1 : 0;
+	f.sequence_end_code_flag = (payload[2] & 0x40) ? 1 : 0;
+	f._video_encode_format = (payload[2] >> 2) & 0x0f;
+	f.reserved = payload[2] & 0x03;
+	CREATE_FILE_NUMBER(dentry, &f, still_picture_flag);
+	CREATE_FILE_NUMBER(dentry, &f, sequence_end_code_flag);
+
+	switch (f._video_encode_format) {
+		case 0: sprintf(video_fmt, "1080p");
+				break;
+		case 1: sprintf(video_fmt, "1080i");
+				break;
+		case 2: sprintf(video_fmt, "720p");
+				break;
+		case 3: sprintf(video_fmt, "480p");
+				break;
+		case 4: sprintf(video_fmt, "480i");
+				break;
+		case 5: sprintf(video_fmt, "240p");
+				break;
+		case 6: sprintf(video_fmt, "120p");
+				break;
+		default:
+				sprintf(video_fmt, "Reserved");
+	}
+	sprintf(f.video_encode_format, "%s [%#x]", video_fmt, f._video_encode_format);
+	CREATE_FILE_STRING(dentry, &f, video_encode_format, XATTR_FORMAT_STRING_AND_NUMBER);
+
+    return 0;
 }
 
