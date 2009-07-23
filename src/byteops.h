ae80ff3ab5c66418b4807a60ca926d9fe0b31e8e
lucasvr <lucasvr@ac0b248e-9ced-4581-99a4-cae81f09c613>
2009-07-23 05:17:56 +0000

    Byteops done right.

--- src/byteops.h
+++ src/byteops.h
@@ -1,9 +1,9 @@
 #ifndef __byteops_h
 #define __byteops_h
 
-#define CONVERT_TO_16(a,b)       ((((uint16_t)a)<<8) | ((uint8_t)b))
-#define CONVERT_TO_24(a,b,c)     ((((uint32_t)a)<<16) | (((uint16_t)b)<<8) | ((uint8_t)c))
-#define CONVERT_TO_32(a,b,c,d)   ((((uint32_t)a)<<24) | (((uint32_t)b)<<16) | (((uint16_t)c)<<8) | ((uint8_t)d))
-#define CONVERT_TO_40(a,b,c,d,e) ((((uint64_t)a)<<32) | (((uint32_t)b)<<24) | (((uint32_t)c)<<16) | ((uint16_t)d)<<8 | ((uint8_t)e))
+#define CONVERT_TO_16(a,b)       (((((uint16_t)a)<<8) & 0xff00) | ((uint8_t)b & 0xff))
+#define CONVERT_TO_24(a,b,c)     (((((uint32_t)a)<<16) & 0xff0000) | CONVERT_TO_16(b,c))
+#define CONVERT_TO_32(a,b,c,d)   (((((uint32_t)a)<<24) & 0xff000000) | CONVERT_TO_24(b,c,d))
+#define CONVERT_TO_40(a,b,c,d,e) (((((uint64_t)a)<<32) & 0xff00000000) | CONVERT_TO_32(b,c,d,e))
 
 #endif /* __byteops_h */
