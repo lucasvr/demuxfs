dc134951eae3142233178535470a13427ab028a8
Lucas C. Villa Real <lucasvr@gmail.com>
2020-07-12 20:19:10 -0300

    DII: handle programs with more than one given name.
    Refs #3

--- src/dsm-cc/dii.c
+++ src/dsm-cc/dii.c
@@ -239,8 +239,7 @@ int dii_create_filesystem(const struct ts_header *header, struct dii_table *dii,
 				if (! ait_dentry)
 					break;
 
-				/* TODO: what if there's more than one application name? */
-				sprintf(buf, "/Application_Name_Descriptor/Application_Name_01/application_name");
+				sprintf(buf, "/Application_Name_Descriptor/Application_Name_%02d/application_name", i);
 				ait_dentry = fsutils_get_dentry(ait_dentry, buf);
 				if (ait_dentry) {
 					app_dentry = CREATE_DIRECTORY(dsmcc_dentry, "%s", ait_dentry->contents);
